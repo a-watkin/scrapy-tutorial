4c0c19f46093ec34d3be66be2e2433acf3518a11
adam <atomicpenguines@gmail.com>
2018-08-16 23:08:19 +0100

    added comments to quotes_spider.py

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -2,7 +2,9 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+    # name of spider must be unique within a project
     name = "quotes"
+
 
     def start_requests(self):
         urls = [
@@ -14,6 +16,11 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        """
+        The parse() method usually parses the response, 
+        extracting the scraped data as dicts and also 
+        finding new URLs to follow and creating new requests (Request) from them.
+        """
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
